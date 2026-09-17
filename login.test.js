af5eeb4c5b0bf33257e4386892c0db980e0df2fb
qthaizzz <quocthaideptrai4264@gmail.com>
2026-09-17 19:31:31 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,15 +1,21 @@
 const login = require("./login");
 
-describe("login()", () => {
-  test("Đăng nhập đúng", () => {
-    expect(login("admin", "123")).toBe(true);
-  });
+describe("Kiểm tra hàm login", () => {
 
-  test("Sai password", () => {
-    expect(login("admin", "wrong")).toBe(false);
-  });
+    test("Đăng nhập đúng username và password phải trả về true", () => {
+        expect(login("admin", "123")).toBe(true);
+    });
 
-  test("Sai username", () => {
-    expect(login("user", "123")).toBe(false);
-  });
+    test("Sai password phải trả về false", () => {
+        expect(login("admin", "456")).toBe(false);
+    });
+
+    test("Sai username phải trả về false", () => {
+        expect(login("user", "123")).toBe(false);
+    });
+
+    test("Sai cả username và password phải trả về false", () => {
+        expect(login("user", "456")).toBe(false);
+    });
+
 });
